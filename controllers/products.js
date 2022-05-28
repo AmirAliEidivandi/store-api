157539aeafb6250893776671c8c11a41c4f5fe3c
AmirAliEidivandi <amirah648887@gmail.com>
2022-05-28 23:07:11 +0430

    first commit

---
+++ controllers/products.js
@@ -0,0 +1,26 @@
+const Product = require("../models/product");
+
+const getAllProductsStatic = async (req, res) => {
+    const products = await Product.find({ price: { $gt: 30 } })
+        .sort("price")
+        .select("name price");
+    res.status(200).json({ products, nbHits: products.length });
+};
+
+const getAllProducts = async (req, res) => {
+    const { featured } = req.query;
+    const queryObject = {};
+
+    if (featured) {
+        queryObject.featured = featured === "true" ? true : false;
+    }
+    console.log(queryObject);
+
+    const products = await Product.find(queryObject);
+    res.status(200).json({ products, nbHits: products.length });
+};
+
+module.exports = {
+    getAllProductsStatic,
+    getAllProducts,
+};
